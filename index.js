ac99eb9136aec709b34c0670fbcf069cd27fd73b
Evgeni Khoptiar <evgeni.khoptiar@emg-soft.com>
2021-05-08 13:40:12 +0300

    db await

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const app = new express();
 const mongoose = require("mongoose");
 const config = require("config");
+const fileUpload = require('express-fileupload')
 const apiRouter = require("./routes/index");
 const errorHandler = require("./middleware/errorHandlingMiddleware");
 const path = require('path');
 const cors = require('cors')
 
+const app = new express();
+
 app.use(cors())
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use("/ada-api", apiRouter);
 app.use(errorHandler);
 
@@ -17,7 +20,7 @@ const port = config.get("serverPort") || 8060;
 
 const start = async () => {
   try {
-    mongoose.connect(config.get("dbUrl"), { 
+    await mongoose.connect(config.get("dbUrl"), { 
       useNewUrlParser: true, 
       useUnifiedTopology: true, 
       useCreateIndex: true,
@@ -27,7 +30,7 @@ const start = async () => {
       console.log("Mock Server started on port ", port);
     });
   } catch (error) {
-    console.log("Error", error);
+    console.log("Error starting server", error);
   }
 };
 start();
